5abe2dcc7e27a3d8896f12c7a24bdae838ce16ce
santos <santosrecovery@gmail.com>
2020-05-07 06:09:19 +0200

    Updates

---
+++ precache-manifest.776b196baa79af1750f9fcdb5ec7ec01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44eaf7f46485873928591c6cbdc21d66",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "124c89e2a0d82b2d12c7",
+    "url": "/Weather-App/static/css/main.dc043f7e.chunk.css"
+  },
+  {
+    "revision": "c86f00ebb15cd54e8f10",
+    "url": "/Weather-App/static/js/2.b8faaabe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Weather-App/static/js/2.b8faaabe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "124c89e2a0d82b2d12c7",
+    "url": "/Weather-App/static/js/main.837b5055.chunk.js"
+  },
+  {
+    "revision": "7ca169c5954aadd1c03e",
+    "url": "/Weather-App/static/js/runtime-main.f2cc568d.js"
+  }
+]);
